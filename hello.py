931b306bbf56ab33eb6f5b7096695676c68c5395
Evans Doe Ocansey <evans@aims.ac.za>
2014-04-24 13:48:33 +0000

    Added a code for printing files in current directory.

--- hello.py
+++ hello.py
@@ -5,4 +5,7 @@ print "Hello World"
 print "My name is Evans"
 # Working with list of files
 CUR_DIR='/Users/evans/Desktop/aims_gh-research-project'
-print gl.glob("*.tex")
+# Printing all files in a the current directory
+ALL_FILES gl.glob("*.*")
+for f in ALL_FILES:
+	print f
